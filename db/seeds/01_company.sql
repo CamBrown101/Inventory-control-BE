5e9cb0d0af125b3ca7579ba8d7eaa8d5aea301d1
cambrown101 <cameron-brown@live.com>
2021-03-24 15:38:44 -0700

    ADD company dummy data

---
+++ db/seeds/01_company.sql
@@ -0,0 +1,3 @@
+INSERT INTO company (company_name, contact_name, email, phone_number)
+VALUES('The Donnelly Group', 'Trevor Kallies', 'tk@donnellygroup.com', 12505750908),
+('Some Other Company', 'Cam Brown', 'cameron-brown@live.com', 2505750908)
