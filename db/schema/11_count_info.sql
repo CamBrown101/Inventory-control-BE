b1c64cdcaaa6f27969d580d43e4212b27b16477c
cambrown101 <cameron-brown@live.com>
2021-03-24 15:02:06 -0700

    Edit and Create count_info table in database

--- db/schema/11_count_info.sql
+++ db/schema/11_count_info.sql
@@ -1,8 +1,8 @@
 CREATE TABLE count_info (
     id SERIAL PRIMARY KEY NOT NULL,
-    venue INTEGER REFRENCES venue(id),
+    venue INTEGER REFERENCES venue(id),
     date_started TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP(),
     date_last_updated TIMESTAMPTZ DEFAULT CLOCK_TIMESTAMP(),
-    started_by INTEGER REFRENCES user(id) NOT NULL,
-    last_updated_by INTEGER REFRENCES user(id) NOT NULL,
+    started_by INTEGER REFERENCES users(id) NOT NULL,
+    last_updated_by INTEGER REFERENCES users(id) NOT NULL
 )
