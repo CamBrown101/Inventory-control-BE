334636f90531690b83992986b4178d5e7d0b12b6
cambrown101 <cameron-brown@live.com>
2021-03-23 21:02:40 -0700

    Create company table

---
+++ db/schema/02_company.sql
@@ -0,0 +1,7 @@
+CREATE TABLE company (
+  id SERIAL PRIMARY KEY NOT NULL,
+  company_name VARCHAR(255) NOT NULL,
+  contact_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone_number INTEGER NOT NULL,
+)
