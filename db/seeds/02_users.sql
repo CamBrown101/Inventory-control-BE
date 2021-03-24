23a360a8482b9dc79eb4f27f757eb9fe1b6569a6
cambrown101 <cameron-brown@live.com>
2021-03-24 15:38:50 -0700

    Add users dummy data

---
+++ db/seeds/02_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, email, password, company_id, phone_number, is_admin, is_active, permission_level)
+VALUES('Cam', 'Brown', 'cameron-brown@live.com', '1234', 1, '2505750908', TRUE, TRUE, 999),
+('Trevor', 'Kallies', 'tk@donnellygroup.ca', '1234', 1, '2505750908', TRUE, TRUE, 999)
+
